b9d27fb67f786670da9af6a264107971687f79fe
aki <60290657+ShojiAkifumi@users.noreply.github.com>
2022-03-19 15:38:08 +0900

    コマンドライン引数を受け取る

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,10 +1,13 @@
+from email.policy import default
 import click
 
 @click.command()
-def cli():
-    lgtm()
-    click.echo('lgtm')
+@click.option('--message', '-m', default='LGTM', show_default=True, help='画像に乗せる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
+    lgtm(keyword, message)
+    click.echo('lgtm')#動作確認用
 
-def lgtm():
+def lgtm(keyword, message):
 
     pass
